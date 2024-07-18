8b89f0a141d8736aa9113eab1fda1d6447acda94
lissakawasaki <lissakawasaki@outlook.com>
2024-07-18 09:31:04 -0300

    Aula Estrutura de Dados 18-07

--- 2o_Ano/Estrutura de Dados/Aula18-07/Fila.c
+++ 2o_Ano/Estrutura de Dados/Aula18-07/Fila.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
-#include <stdlib.h> 
-#include <stdbool.h>
+#include <stdlib.h>
 
 // Implementação de fila
 
 typedef struct no {
     int valor;
     struct no *proximo;
-} No; 
+} No;
 
 typedef struct {
     No *inicio;
@@ -22,44 +21,46 @@ void criarFila(Fila *fila){ // Criando uma fila
 }
 
 void inserirFila(Fila *fila, int num){
-    No *novo = malloc (sizeof(No));
-        if(novo == NULL){
-            novo->valor = num;
-            novo->proximo = NULL;
-            if (fila->inicio == NULL){
-                fila->inicio = novo;
-                fila->final = novo;
-            } else {
-                fila->final->proximo = novo;
-                fila->final = novo;
-            }
-                fila->tamanho++;
-
+    No *novo = malloc(sizeof(No));
+    if(novo != NULL){
+        novo->valor = num;
+        novo->proximo = NULL;
+        if (fila->inicio == NULL){
+            fila->inicio = novo;
+            fila->final = novo;
         } else {
-            printf("\n Erro ao alocar memória.");
+            fila->final->proximo = novo;
+            fila->final = novo;
         }
+        fila->tamanho++;
+    } else {
+        printf("\nErro ao alocar memória.\n");
     }
+}
 
 No* removerFila(Fila *fila){
     No *remover = NULL;
-    if(fila->inicio){
+    if(fila->inicio != NULL){
         remover = fila->inicio;
-        fila->inicio = remover -> proximo;
+        fila->inicio = remover->proximo;
         fila->tamanho--;
+        if (fila->inicio == NULL) {
+            fila->final = NULL;
+        }
     } else {
-        printf("\t Fila Vazia! \n");
-        return remover;
+        printf("\tFila Vazia!\n");
     }
-}    
+    return remover;
+}
 
 void imprimirFila(Fila *fila){
     No *aux = fila->inicio;
-    printf("\t ----FILA---- \n");
-        while(aux = true){
-            printf("%d", aux->valor);
-            aux = aux->proximo;
-        }
-    printf("\t ----FIM DA FILA---- \n");
+    printf("\t----FILA----\n");
+    while(aux != NULL){
+        printf("%d ", aux->valor);
+        aux = aux->proximo;
+    }
+    printf("\n\t----FIM DA FILA----\n");
 }
 
 int main(){
@@ -69,30 +70,32 @@ int main(){
     int valor, entradaUsuario;
 
     do {
-        printf("0 - sair \n 1 - Inserir \n 2 - Remover \n 3 - Imprimir \n");
-        scanf("%d0, &entradaUsuario");
-        switch (entradaUsuario)
+        printf(" 0 - Sair \n 1 - Inserir \n 2 - Remover \n 3 - Imprimir \n");
+        scanf("%d", &entradaUsuario);
+        switch(entradaUsuario)
         {
         case 1:
-        printf("Digite um valor a ser inserido na fila: ");
-        scanf("%d", &valor);
-        inserirFila(&fila, valor);
+            printf("Digite um valor a ser inserido na fila: ");
+            scanf("%d", &valor);
+            inserirFila(&fila, valor);
             break;
         case 2:
-        printf("O valor indicado pelo indice sera removido.\n");
-        r = removerFila(&fila);
-        printf("Removido: %d\n", r->valor);
-        free(r);
+            r = removerFila(&fila);
+            if (r != NULL) {
+                printf("Removido: %d\n", r->valor);
+                free(r);
+            }
             break;
         case 3:
-        imprimirFila(&fila);
+            imprimirFila(&fila);
             break;
         default:
             if(entradaUsuario != 0){
-                printf("Opção inválida.");
+                printf("Opção inválida.\n");
             }
             break;
         }
     } while (entradaUsuario != 0);
-        return 0;
+
+    return 0;
 }
